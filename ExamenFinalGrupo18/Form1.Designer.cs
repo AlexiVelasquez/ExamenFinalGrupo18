7d24c233e0862ff470da348c4faff47e94594c9e
ishigodk <ishigodk@gmail.com>
2024-07-07 22:21:51 -0500

    Agregue interfaz

--- ExamenFinalGrupo18/Form1.Designer.cs
+++ ExamenFinalGrupo18/Form1.Designer.cs
@@ -28,21 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txt_dato = new System.Windows.Forms.TextBox();
+            this.list_Dato = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_ingreso = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txt_dato
+            // 
+            this.txt_dato.Location = new System.Drawing.Point(102, 17);
+            this.txt_dato.Name = "txt_dato";
+            this.txt_dato.ShortcutsEnabled = false;
+            this.txt_dato.Size = new System.Drawing.Size(100, 22);
+            this.txt_dato.TabIndex = 0;
+            this.txt_dato.TabStop = false;
+            // 
+            // list_Dato
+            // 
+            this.list_Dato.FormattingEnabled = true;
+            this.list_Dato.ItemHeight = 16;
+            this.list_Dato.Location = new System.Drawing.Point(238, 66);
+            this.list_Dato.Name = "list_Dato";
+            this.list_Dato.Size = new System.Drawing.Size(210, 196);
+            this.list_Dato.TabIndex = 1;
+            this.list_Dato.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese letra:";
+            // 
+            // btn_ingreso
+            // 
+            this.btn_ingreso.Location = new System.Drawing.Point(46, 66);
+            this.btn_ingreso.Name = "btn_ingreso";
+            this.btn_ingreso.Size = new System.Drawing.Size(75, 23);
+            this.btn_ingreso.TabIndex = 3;
+            this.btn_ingreso.Text = "Ingresar";
+            this.btn_ingreso.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(485, 334);
+            this.Controls.Add(this.btn_ingreso);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.list_Dato);
+            this.Controls.Add(this.txt_dato);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_dato;
+        private System.Windows.Forms.ListBox list_Dato;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_ingreso;
     }
 }
 
