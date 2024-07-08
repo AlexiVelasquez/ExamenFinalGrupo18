186ca6b9c6135849fdeda86571fb0de0d5258e26
alexi <velasquezacunaalexi@gmail.com>
2024-07-08 12:30:38 -0500

    Arreglar Enunciado2

---
+++ Enunciado2/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Enunciado2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.dgvRegistroVideojuego = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonRegistrar = new System.Windows.Forms.Button();
+            this.rbtnPerdió = new System.Windows.Forms.RadioButton();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.RButtGane = new System.Windows.Forms.RadioButton();
+            this.textNewEliminar = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblPuntosTotales = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistroVideojuego)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.buttonEliminar);
+            this.groupBox1.Controls.Add(this.dgvRegistroVideojuego);
+            this.groupBox1.Controls.Add(this.buttonRegistrar);
+            this.groupBox1.Controls.Add(this.rbtnPerdió);
+            this.groupBox1.Controls.Add(this.buttonSalir);
+            this.groupBox1.Controls.Add(this.RButtGane);
+            this.groupBox1.Controls.Add(this.textNewEliminar);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.lblPuntosTotales);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(44, 39);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(709, 624);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de videojuegos";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(419, 561);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Total de Puntos: ";
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.buttonEliminar.Location = new System.Drawing.Point(371, 238);
+            this.buttonEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(111, 33);
+            this.buttonEliminar.TabIndex = 14;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = false;
+            this.buttonEliminar.Click += new System.EventHandler(this.buttonEliminar_Click);
+            // 
+            // dgvRegistroVideojuego
+            // 
+            this.dgvRegistroVideojuego.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvRegistroVideojuego.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvRegistroVideojuego.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            this.dgvRegistroVideojuego.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvRegistroVideojuego.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvRegistroVideojuego.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.HotTrack;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRegistroVideojuego.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dgvRegistroVideojuego.ColumnHeadersHeight = 30;
+            this.dgvRegistroVideojuego.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvRegistroVideojuego.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvRegistroVideojuego.EnableHeadersVisualStyles = false;
+            this.dgvRegistroVideojuego.GridColor = System.Drawing.Color.SteelBlue;
+            this.dgvRegistroVideojuego.Location = new System.Drawing.Point(39, 304);
+            this.dgvRegistroVideojuego.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvRegistroVideojuego.Name = "dgvRegistroVideojuego";
+            this.dgvRegistroVideojuego.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle11.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle11.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle11.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRegistroVideojuego.RowHeadersDefaultCellStyle = dataGridViewCellStyle11;
+            this.dgvRegistroVideojuego.RowHeadersWidth = 51;
+            dataGridViewCellStyle12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.Color.White;
+            this.dgvRegistroVideojuego.RowsDefaultCellStyle = dataGridViewCellStyle12;
+            this.dgvRegistroVideojuego.Size = new System.Drawing.Size(569, 241);
+            this.dgvRegistroVideojuego.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "VideoJuego";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 138;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Fecha";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 89;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Gané o Perdí";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 147;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Puntos";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 92;
+            // 
+            // buttonRegistrar
+            // 
+            this.buttonRegistrar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.buttonRegistrar.Location = new System.Drawing.Point(289, 135);
+            this.buttonRegistrar.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonRegistrar.Name = "buttonRegistrar";
+            this.buttonRegistrar.Size = new System.Drawing.Size(111, 33);
+            this.buttonRegistrar.TabIndex = 3;
+            this.buttonRegistrar.Text = "Registrar";
+            this.buttonRegistrar.UseVisualStyleBackColor = false;
+            this.buttonRegistrar.Click += new System.EventHandler(this.buttonRegistrar_Click_1);
+            // 
+            // rbtnPerdió
+            // 
+            this.rbtnPerdió.AutoSize = true;
+            this.rbtnPerdió.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rbtnPerdió.Location = new System.Drawing.Point(559, 90);
+            this.rbtnPerdió.Margin = new System.Windows.Forms.Padding(4);
+            this.rbtnPerdió.Name = "rbtnPerdió";
+            this.rbtnPerdió.Size = new System.Drawing.Size(89, 20);
+            this.rbtnPerdió.TabIndex = 6;
+            this.rbtnPerdió.TabStop = true;
+            this.rbtnPerdió.Text = "Perdí (-20)";
+            this.rbtnPerdió.UseVisualStyleBackColor = true;
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.buttonSalir.Location = new System.Drawing.Point(559, 238);
+            this.buttonSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(111, 33);
+            this.buttonSalir.TabIndex = 10;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = false;
+            // 
+            // RButtGane
+            // 
+            this.RButtGane.AutoSize = true;
+            this.RButtGane.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RButtGane.Location = new System.Drawing.Point(559, 34);
+            this.RButtGane.Margin = new System.Windows.Forms.Padding(4);
+            this.RButtGane.Name = "RButtGane";
+            this.RButtGane.Size = new System.Drawing.Size(100, 20);
+            this.RButtGane.TabIndex = 2;
+            this.RButtGane.TabStop = true;
+            this.RButtGane.Text = "Gané (+100)";
+            this.RButtGane.UseVisualStyleBackColor = true;
+            // 
+            // textNewEliminar
+            // 
+            this.textNewEliminar.Location = new System.Drawing.Point(336, 190);
+            this.textNewEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.textNewEliminar.Name = "textNewEliminar";
+            this.textNewEliminar.Size = new System.Drawing.Size(332, 22);
+            this.textNewEliminar.TabIndex = 12;
+            this.textNewEliminar.TextChanged += new System.EventHandler(this.textNewEliminar_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(232, 33);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(248, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // lblPuntosTotales
+            // 
+            this.lblPuntosTotales.AutoSize = true;
+            this.lblPuntosTotales.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPuntosTotales.Location = new System.Drawing.Point(35, 37);
+            this.lblPuntosTotales.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPuntosTotales.Name = "lblPuntosTotales";
+            this.lblPuntosTotales.Size = new System.Drawing.Size(160, 16);
+            this.lblPuntosTotales.TabIndex = 5;
+            this.lblPuntosTotales.Text = "Nombre del Videojuego : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(35, 190);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ingrese Videojuego a Eliminar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(35, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Fecha :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Location = new System.Drawing.Point(232, 90);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(761, 688);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistroVideojuego)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonEliminar;
+        private System.Windows.Forms.DataGridView dgvRegistroVideojuego;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button buttonRegistrar;
+        private System.Windows.Forms.RadioButton rbtnPerdió;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.RadioButton RButtGane;
+        private System.Windows.Forms.TextBox textNewEliminar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblPuntosTotales;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
